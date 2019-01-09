cf2dab9a8428e02ea435263cd063900c6f2bcc7d
VeselAbaya <Vandervise465@gmail.com>
2019-01-09 13:59:09 +0300

    refactoring(lol) :>

--- main.js
+++ main.js
@@ -3,14 +3,9 @@ const path = require('path')
 
 const {app, BrowserWindow, ipcMain} = electron
 
-const blizzardRegistrationUrl = 'https://eu.battle.net/account/creation/ru/?country=RUS&firstName=&secondName=&birthDay=&birthMonth=1&birthYear=&question=0'
-
 let mainWindow
-let registrationForm
 
 app.on('ready', () => {
     mainWindow = new BrowserWindow
     mainWindow.loadURL(`file://${__dirname}/mainWindow.html`)
-
-    mainWindow.webContents.send('blizzard-form:fill', 'aaaaa')
 })
